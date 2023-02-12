8554b0fd14db4bdb29f09520371dda601390762c
Abhishek Kumar Gupta <107269233+TechieAbhishekGupta@users.noreply.github.com>
2023-02-12 15:07:08 +0530

    Area of Circle
    Adding 1. Write a program that calculates the area of a circle given its radius.

---
+++ areaOfCircle.cs
@@ -0,0 +1,10 @@
+﻿// 1. Write a program that calculates the area of a circle given its radius.
+
+Console.Write("Enter the Radius: ");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+double area = (3.14) * radius * radius;
+
+Console.WriteLine($"Area of Circle: {Math.Round(area,2)}");
+
+Console.ReadKey();
